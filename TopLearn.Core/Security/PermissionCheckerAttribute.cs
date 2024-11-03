8063fb9ad9669f2caa9d77c44af6b4fd4852455f
P-pouria <pouria13832077@gmail.com>
2024-11-03 18:31:05 +0330

    Change PermissionCheckerAttribute

--- TopLearn.Core/Security/PermissionCheckerAttribute.cs
+++ TopLearn.Core/Security/PermissionCheckerAttribute.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
@@ -10,24 +8,24 @@ namespace TopLearn.Core.Security
 {
     public class PermissionCheckerAttribute : AuthorizeAttribute, IAuthorizationFilter
     {
+        private int _permissionId;
         private IPermissionService _permissionService;
-        private int _permissionId = 0;
-        public PermissionCheckerAttribute(int permissionId)
+
+        public PermissionCheckerAttribute(int permissionId, IPermissionService permissionService)
         {
             _permissionId = permissionId;
+            _permissionService = permissionService ?? throw new ArgumentNullException(nameof(permissionService));
         }
 
         public void OnAuthorization(AuthorizationFilterContext context)
         {
-            _permissionService =
-                (IPermissionService)context.HttpContext.RequestServices.GetService(typeof(IPermissionService));
-            if (context.HttpContext.User.Identity.IsAuthenticated)
+            if (context.HttpContext.User.Identity?.IsAuthenticated ?? false)
             {
                 string userName = context.HttpContext.User.Identity.Name;
 
                 if (!_permissionService.Checkpermission(_permissionId, userName))
                 {
-                    context.Result = new RedirectResult("/Login?" + context.HttpContext.Request.Path);
+                    context.Result = new RedirectResult($"/Login?{context.HttpContext.Request.Path}");
                 }
             }
             else
